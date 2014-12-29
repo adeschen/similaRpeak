a3ea53d64380ab8732cdb4324354229e54e67ab5
adeschen <adeschen@hotmail.com>
2014-12-29 01:30:48 -0500

    Correcting tests to fix new code.

--- inst/unitTests/test_metrics.R
+++ inst/unitTests/test_metrics.R
@@ -24,7 +24,8 @@ factory = MetricFactory$new()
 test.metrics_metricfactory_all <- function() {
     exp <- list("RATIO_AREA" = 1.084291188, "DIFF_POS_MAX" = 2,
                 "RATIO_MAX_MAX" = 1.413043478, 
-                "RATIO_INTERSECT" = 0.346534653)
+                "RATIO_INTERSECT" = 0.346534653,
+                "RATIO_NORMALIZED_INTERSECT" = 0.343525474)
     obs <- factory$createMetric("ALL", c(1,59,6,24,65,34,15,4,53,22), 
                                 c(15,9,46,44,9,39,27,34,34,4))
     checkEquals(obs, exp, tolerance = .Machine$double.eps^0.5)
@@ -71,8 +72,8 @@ test.metrics_metricfactory_all_na_only <- function() {
 
 ## Test the result of DIFF_POS_MAX when using profiles with only zero values
 test.metrics_metricfactory_all_0_diff_pos_max <- function() {
-    exp <- list("RATIO_AREA"=NA,"DIFF_POS_MAX" = NA,"RATIO_MAX_MAX" = NA,
-                "RATIO_INTERSECT" = NA)
+    exp <- list("RATIO_AREA" = NA,"DIFF_POS_MAX" = NA,"RATIO_MAX_MAX" = NA,
+                "RATIO_INTERSECT" = NA, "RATIO_NORMALIZED_INTERSECT" = NA)
     obs <- factory$createMetric("ALL", c(0,0,0,0), c(0,0,0,0))
     checkEquals(obs$DIFF_POS_MAX, exp$DIFF_POS_MAX, 
                 msg = paste("The DIFF_POS_MAX is not expected value when only ", 
@@ -82,7 +83,7 @@ test.metrics_metricfactory_all_0_diff_pos_max <- function() {
 ## Test the result of RATIO_MAX_MAX when using profiles with only zero values
 test.metrics_metricfactory_all_0_ratio_max_max <- function() {
     exp <- list("RATIO_AREA"=NA,"DIFF_POS_MAX"=NA,"RATIO_MAX_MAX"=NA,
-                "RATIO_INTERSECT"=NA)
+                "RATIO_INTERSECT"=NA, "RATIO_NORMALIZED_INTERSECT" = NA)
     obs <- factory$createMetric("ALL", c(0,0,0,0), c(0,0,0,0))
     checkEquals(obs$RATIO_MAX_MAX, exp$RATIO_MAX_MAX, 
                 msg = paste("The RATIO_MAX_MAX is not expected value when only ",
@@ -92,7 +93,7 @@ test.metrics_metricfactory_all_0_ratio_max_max <- function() {
 ## Test the result of RATIO_INTERSECT when using profiles with only zero values
 test.metrics_metricfactory_all_0_ratio_intersect <- function() {
     exp <- list("RATIO_AREA"=NA,"DIFF_POS_MAX"=NA,"RATIO_MAX_MAX"=NA,
-                "RATIO_INTERSECT"=NA)
+                "RATIO_INTERSECT"=NA, "RATIO_NORMALIZED_INTERSECT" = NA)
     obs <- factory$createMetric("ALL", c(0,0,0,0), c(0,0,0,0))
     checkEquals(obs$RATIO_INTERSECT, exp$RATIO_INTERSECT, 
                 msg = paste("The RATIO_INTERSECT is not expected value when ",
@@ -102,7 +103,7 @@ test.metrics_metricfactory_all_0_ratio_intersect <- function() {
 ## Test the result of RATIO_AREA when using a profiles with only zero values
 test.metrics_metricfactory_all_0_ratio_area <- function() {
     obs <- list("RATIO_AREA"=NA,"DIFF_POS_MAX"=NA,"RATIO_MAX_MAX"=NA,
-                "RATIO_INTERSECT"=NA)
+                "RATIO_INTERSECT"=NA, "RATIO_NORMALIZED_INTERSECT" = NA)
     exp <- factory$createMetric("ALL", c(0,0,0,0), c(0,0,0,0))
     checkEquals(obs$RATIO_AREA, exp$RATIO_AREA, 
                 msg = paste("The RATIO_AREA is not expected value when only ",
@@ -112,7 +113,8 @@ test.metrics_metricfactory_all_0_ratio_area <- function() {
 ## Test the result of using "ALL" with some NA in profiles
 test.metrics_metricfactory_all_with_some_na <- function() {
     exp <- list("RATIO_AREA"=1.05188679245,"DIFF_POS_MAX"=2,
-                "RATIO_MAX_MAX"=1.41304347826,"RATIO_INTERSECT"=0.40776699)
+                "RATIO_MAX_MAX"=1.41304347826,"RATIO_INTERSECT"=0.40776699,
+                "RATIO_NORMALIZED_INTERSECT" = 0.40445316)
     obs <- factory$createMetric("ALL", c(NA,NA,6,24,65,34,15,4,53,22), 
                                 c(NA,9,46,44,9,39,27,NA,34,4))
     checkEquals(obs, exp, tolerance = .Machine$double.eps^0.5)
